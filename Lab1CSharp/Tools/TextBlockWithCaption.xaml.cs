56fc7a348e36f5b3967269fecd21e85d7fd6ddfa
Andrii Dubovyk <and.dubovyk@gmail.com>
2022-02-10 19:02:55 +0200

    Add textblock with caption to show age

---
+++ Lab1CSharp/Tools/TextBlockWithCaption.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab1CSharp.Tools
+{
+    /// <summary>
+    /// Interaction logic for TextBlockWithCaption.xaml
+    /// </summary>
+    public partial class TextBlockWithCaption : UserControl
+    {
+        public TextBlockWithCaption()
+        {
+            InitializeComponent();
+        }
+
+        public string Caption
+        {
+            get
+            {
+                return TbCaption.Text;
+            }
+            set
+            {
+                TbCaption.Text = value;
+            }
+        }
+
+        public string Text
+        {
+            get
+            {
+                return TbValue.Text;
+            }
+            set
+            {
+                TbValue.Text=value;
+            }
+        }
+    }
+}
