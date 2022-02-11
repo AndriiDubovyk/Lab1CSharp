90947674cd925d94e9fe7851246020d9f1496917
Andrii Dubovyk <and.dubovyk@gmail.com>
2022-02-11 12:46:55 +0200

    Add simple action for OK button

--- Lab1CSharp/ViewModels/BirthdateAnalyserViewModel.cs
+++ Lab1CSharp/ViewModels/BirthdateAnalyserViewModel.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using Lab1CSharp.Models;
+using Lab1CSharp.Tools;
 
 namespace Lab1CSharp.ViewModels
 {
     class BirthdateAnalyserViewModel
     {
+        #region Fields
+        private BirthdateAnalyser _birthdateAnalyser;
+        private RelayCommand<object> _okCommand;
+        #endregion
+
+        public RelayCommand<object> OKCommand
+        {
+            get
+            {
+                return _okCommand ??= new RelayCommand<object>(_ => OK(), CanExecute);
+            }
+        }
+
+        private void OK()
+        {
+            MessageBox.Show($"OK");
+        }
+
+        private bool CanExecute(object obj)
+        {
+            return true;
+        }
+
     }
 }
